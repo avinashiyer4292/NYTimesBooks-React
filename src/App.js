7c8ff6bdbf4db380819c1ff4595e199487f0d71b
avinashiyer <avinashiyer4292@gmail.com>
2018-08-18 00:54:04 -0700

    Fetching JSONP response

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import * as constants from './constants/constants.js';
+import { connect } from 'react-redux';
+import BooksList from './components/BooksList';
+import fetchJsonP from 'fetch-jsonp';
 class App extends Component {
+  componentDidMount(){
+    fetchJsonP(`${constants.BOOKS_API_URL}`, {
+            jsonpCallbackFunction: 'foobar',
+          })
+          .then(res => res.json())
+          .then(json => console.log(`JSON IS: ${JSON.stringify(json)}`))
+          .then(json => console.log(JSON.stringify(json)))
+          .catch(err => {  throw new Error(err) });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <BooksList />
     );
   }
 }
