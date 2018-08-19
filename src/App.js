ef7db771e9c961729ddf90f49df81724ec6334c6
avinashiyer <avinashiyer4292@gmail.com>
2018-08-19 13:22:33 -0700

    Updated App with router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import BookCategories from './components/BookCategories';
+import Book from './components/Book';
 import Header from './components/Header';
+import { Route } from 'react-router-dom';
 class App extends Component {
   
   render() {
     return (
-      <div>
-        <Header />
-        <BookCategories  />
-      </div>
+      
+        <div>
+          <Header />
+        
+            <Route exact strict path='/' component={BookCategories}></Route>
+            <Route path='/book' component={Book}></Route>
+        </div>
+
     );
   }
 }
