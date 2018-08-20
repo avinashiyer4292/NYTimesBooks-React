f7186b53f133014c99bc9b365f9c32f623602602
avinashiyer <avinashiyer4292@gmail.com>
2018-08-19 18:39:10 -0700

    Removed service worker to test fetch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -17,4 +16,3 @@ ReactDOM.render(
             </BrowserRouter>
         </Provider>  
     , document.getElementById('root'));
-registerServiceWorker();
