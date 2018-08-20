8424bc5f26406dcf5d706d21768ef36eb97ac1f1
avinashiyer <avinashiyer4292@gmail.com>
2018-08-19 18:52:58 -0700

    Changed http to https

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const API_KEY=`db274442db1d4321b661691c0ded7260`;
 export const CALLBACK_FUNCTION=`foobar`;
-export const BOOKS_API_URL=`http://api.nytimes.com/svc/books/v3/lists/overview.jsonp?callback=foobar&api-key=${API_KEY}`
+export const BOOKS_API_URL=`https://api.nytimes.com/svc/books/v3/lists/overview.jsonp?callback=foobar&api-key=${API_KEY}`
