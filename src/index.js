7ccb501300d077940387512ff980720ae9a16607
avinashiyer <avinashiyer4292@gmail.com>
2018-08-19 17:20:26 -0700

    master rebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-<<<<<<< HEAD
 import reducer from './store/reducer';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -18,8 +17,4 @@ ReactDOM.render(
             </BrowserRouter>
         </Provider>  
     , document.getElementById('root'));
-=======
-
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> create-react-app setup
 registerServiceWorker();
